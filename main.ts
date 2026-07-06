f761739f4117e645415af7b68ce263337072f062
Lucas <lukasilverio94>
2026-07-06 15:17:36 -0300

    chore: add and use dependencies

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
 // @deno-types='npm:@types/express'
-import express, { Request, Response } from 'npm:express';
+import express, { Request, Response, NextFunction } from "npm:express";
+// @deno-types='npm:@types/morgan'
+import morgan from "npm:morgan";
 import { connectMongoDB } from "./src/config/db.ts";
-import { UserRouter } from "./src/routes/userRoutes.ts";
+import { UserRouter } from "./src/routes/UserRoutes.ts";
+import responser from "responser";
 
 const app = express();
 const PORT = Number(Deno.env.get("PORT")) || 3000;
 
 await connectMongoDB();
 
+app.use(morgan('dev'));
 app.use(express.json());
+app.use(responser.default);
 
 app.use(UserRouter);
-
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Welcome to the Deno Blog API!");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
