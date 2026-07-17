36d4a2d7756a609aaf000c284bc7dbbf0349cc62
Lucas <lukasilverio94>
2026-07-17 15:59:31 -0300

    feat: add cors middleware

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 import express from "express";
 // @deno-types='npm:@types/morgan'
 import morgan from "morgan";
+import cors from 'cors';
 import { connectMongoDB } from "./src/config/db.ts";
 import { UserRouter } from "./src/features/users/UserRoutes.ts";
 import responser from "responser";
@@ -11,10 +12,17 @@ import { AuthRouter } from "./src/features/auth/AuthRoutes.ts";
 import { errorHandler } from "./src/middlewares/Error.ts";
 
 const app = express();
+const corsOptions = {
+  origin:  Deno.env.get("BASE_CLIENT_URL"), 
+  credentials: true,            
+  optionsSuccessStatus: 200    
+}
+
 const PORT = Number(Deno.env.get("PORT")) || 3000;
 
 await connectMongoDB();
 
+app.use(cors(corsOptions));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(responser.default);
